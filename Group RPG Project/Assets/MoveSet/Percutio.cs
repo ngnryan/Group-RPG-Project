a84554b4806b89efe67b60af2d2cfe2c96a91489
ngnryan <153254061+ngnryan@users.noreply.github.com>
2025-11-20 19:48:50 -0500

    Intitial

--- Group RPG Project/Assets/MoveSet/Percutio.cs
+++ Group RPG Project/Assets/MoveSet/Percutio.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Basic : MonoBehaviour
 {
+    private int attack;
+    private int accuracy;
+    private int speed;
+    private int energyCost;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

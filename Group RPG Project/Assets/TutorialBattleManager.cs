082af3e5e60a24f17b16111f44781dfee7102561
ngnryan <153254061+ngnryan@users.noreply.github.com>
2025-11-22 09:34:48 -0500

    Initializing
    Fight Sprites
    Base Battle Logic (To edit and script w/ story, flesh out stats, etc.)
    Both BattleControllers (Need to implement logic)
    Both CharacterStats (Need to Implement logic)

---
+++ Group RPG Project/Assets/TutorialBattleManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine; //REFRAME LOGIC TO FIT CAT WINNING
+
+public class BattleManager : MonoBehaviour
+{
+    public PlayerBattleController playerBattleController;
+    public EnemyBattleController enemyBattleController;
+
+    private bool playerTurn = true;
+    private void Start()
+    {
+        StartPlayerTurn();
+    }
+
+    public void StartPlayerTurn()
+    {
+        playerTurn = true;
+        Debug.Log("Player's turn!");
+    }
+
+    public void PlayerAttack(int damage)
+    {
+        enemy.TakeDamage(damage);
+
+        if (enemy.stats.currentHP <=0)
+        {
+            EnemyDefeated();
+        }
+        else
+        {
+            StartEnemyTurn();
+        }
+    }
+
+    public void StartEnemyTurn()
+    {
+        playerTurn = false;
+        Debug.Log("Enemy's turrn!");
+
+        Invoke(nameof(EnemyAttack), 1f);
+    }
+
+    private void EnemyAttackAction()
+    {
+        int damage = enemy.stats.attackPower;
+        player.TakeDamage(damage);
+
+        if (player.stats.currenthp <= 0)
+        {
+            PlayerDefeated();
+        }
+        else
+        {
+            StartPlayerTurn();
+        }
+    }
+
+    private void EnemyDefeated()
+    {
+        Debug.Log("Enemy was defeated!"); //drop loot (UI), exit battle
+    }
+    private void PlayerDefeated
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

49cc7687a4c0d8c9024cf4c1f0542ae034bea169
Julian <julianyelisetty@gmail.com>
2025-12-01 21:10:56 -0500

    v25

--- Group RPG Project/Assets/UnderwaterTrigger.cs
+++ Group RPG Project/Assets/UnderwaterTrigger.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class UnderwaterTrigger
+public class UnderwaterTrigger : MonoBehaviour
 {
-    
+    private FogManager fogManager;
+
+    void Start()
+    {
+        fogManager = Object.FindFirstObjectByType<FogManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fogManager.ApplyUnderwaterFog();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fogManager.ApplyDefaultFog();
+        }
+    }
 }
